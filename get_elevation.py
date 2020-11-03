f8b7f59759fc760f2ebbfe95e43fca1c7622a816
gabe <gm.saintangel@gmail.com>
2020-11-03 11:50:48 -0600

    cleaned up code

--- get_elevation.py
+++ get_elevation.py
@@ -3,10 +3,8 @@ import gpxpy.gpx
 from math import sin, cos, sqrt, atan2, radians
 import time
 import plotly.express as px
-
 #Constants
 earth_radius_m = 6373.0 #approx radius of the earth
-
 
 
 def get_distance_two_points(prev_coordinates,current_coordinates):
@@ -25,20 +23,20 @@ def get_distance_two_points(prev_coordinates,current_coordinates):
     distance = earth_radius_m * c
     return distance
 
+
 if __name__ == "__main__":
     gpx_file = open('ASC2020 route.gpx')#open gpx file
-
-    
     gpx = gpxpy.parse(gpx_file) #pass file into gpxpy
 
     prev_coordinates, current_coordinates = [0,0],[0,0] #pairs of lat and lon coordinates
     prev_elevation, current_elevation = 0,0 #compare the difference in elevation over two coordinates
     starting_coordinates, ending_coordinates = [0,0],[0,0] #pairs of lat and lon coordinates for perfoming summaries
-    current_distance = 0
-    total_distance = 0
-    elevation_difference = 0
-    elevation_data = [] # for graphing elevation along route
-    distance_data = []
+    current_distance = 0 #distance from the last starting point
+    total_distance = 0 #total distance of the route
+    elevation_difference = 0 #difference in elevation from the last starting point
+    elevation_data = [] # for graphing elevation along route, y axis
+    distance_data = [] # for graphing elevation along route, x axis
+
     for track in gpx.tracks:
         for segment in track.segments:
             for point in segment.points:
@@ -77,4 +75,4 @@ if __name__ == "__main__":
 #graph data
 fig = px.line(x=distance_data, y=elevation_data, color=px.Constant("Elevation"),
              labels=dict(x="Distance from Start in KM", y="Elevation in M", color="Legend"))
-fig.show()
+fig.show()#show graph in browser
